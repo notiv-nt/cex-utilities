506021470c261e56439a8d2f5e856cda9abce7c2
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2022-11-19 22:43:09 +0300

    Update ui.service.ts

--- src/services/ui/ui.service.ts
+++ src/services/ui/ui.service.ts
@@ -3,10 +3,10 @@ import { extractPriceFromElement, triggerInputChange } from '../../lib';
 
 const SYMBOL_PLACEHOLDER = '--';
 const SYMBOL_SELECTOR = '#app .watch-drop-box .ticker-title';
-const STOP_LOSS_INPUT_SELECTOR = '#app .place-order-form-box .place-order-input-box input[name="slTriggerPx"]';
+const STOP_LOSS_INPUT_SELECTOR = '#app .place-order-form-box input[name="slTriggerPx"]';
 const LAST_PRICE_TOP_BAR_SELECTOR = '#app .trade-header-box .ticker-last-box .last';
-const AMOUNT_INPUT_SELECTOR = '#app .place-order-form-box .place-order-input-box input[name="size"]';
-const PRICE_INPUT_SELECTOR = '#leftPoForm .place-order-input-box [name="price"]';
+const AMOUNT_INPUT_SELECTOR = '#app .place-order-form-box input[name="size"]';
+const PRICE_INPUT_SELECTOR = '#leftPoForm [name="price"], #rightPoForm [name="price"]';
 
 @singleton()
 export class UiService {
@@ -29,28 +29,40 @@ export class UiService {
     return document.querySelector<HTMLInputElement>(STOP_LOSS_INPUT_SELECTOR);
   }
 
+  public getStopLossInputs() {
+    return document.querySelectorAll<HTMLInputElement>(STOP_LOSS_INPUT_SELECTOR);
+  }
+
   public getPriceInput() {
     return document.querySelector<HTMLInputElement>(PRICE_INPUT_SELECTOR);
   }
 
+  public getPriceInputs() {
+    return document.querySelectorAll<HTMLInputElement>(PRICE_INPUT_SELECTOR);
+  }
+
   public changePriceInput(value: number) {
-    const input = this.getPriceInput();
-    if (input) {
-      triggerInputChange(input, value);
-    }
+    this.getPriceInputs().forEach((input) => {
+      if (input) {
+        triggerInputChange(input, value);
+      }
+    });
   }
 
   public changeStopLossInput(value: number) {
-    const input = this.getStopLossInput();
-    if (input) {
-      triggerInputChange(input, value);
-    }
+    this.getStopLossInputs().forEach((input) => {
+      if (input) {
+        triggerInputChange(input, value);
+      }
+    });
   }
 
   public setAmount(amount: number) {
-    const amountInput = document.querySelector<HTMLInputElement>(AMOUNT_INPUT_SELECTOR);
-    if (amountInput) {
-      triggerInputChange(amountInput, amount);
-    }
+    const amountInputs = document.querySelectorAll<HTMLInputElement>(AMOUNT_INPUT_SELECTOR);
+    amountInputs.forEach((input) => {
+      if (input) {
+        triggerInputChange(input, amount);
+      }
+    });
   }
 }
