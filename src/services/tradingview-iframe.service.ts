7c6928637dbbe784130d3fef7f388d80db19130c
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2022-11-29 18:29:52 +0300

    Autofocus tradingview iframe

--- src/services/tradingview-iframe.service.ts
+++ src/services/tradingview-iframe.service.ts
@@ -1,6 +1,10 @@
 import { singleton } from 'tsyringe';
 import { omit } from 'lodash';
 import BaseService from '../base/base.service';
+import { Loop } from '../core/loop';
+import { log } from '../lib/log';
+
+const IFRAME_SELECTOR = '.tv-chart-box > iframe[name*="tradingview_"]';
 
 @singleton()
 export class TradingviewIframeService extends BaseService {
@@ -8,11 +12,19 @@ export class TradingviewIframeService extends BaseService {
 
   lastPrice: number | null = null;
 
+  constructor(private readonly loop: Loop) {
+    super();
+  }
+
   public setupListeners() {
     window.addEventListener('message', (e) => {
       if (e.data.$key === 'OKX_TRADINGVIEW') {
         this.handleMessage(omit(e.data, '$key'));
       }
+    });
+
+    this.loop.on('tick', () => {
+      this.patchIframe();
     });
   }
 
@@ -28,6 +40,19 @@ export class TradingviewIframeService extends BaseService {
 
     if (message.messageType === 'crosshair' && Number.isFinite(message.price)) {
       this.lastPrice = message.price;
+    }
+  }
+
+  private patchIframe() {
+    const iframe = document.querySelector<HTMLIFrameElement>(IFRAME_SELECTOR);
+
+    if (iframe && !iframe.getAttribute('data-okx-patched')) {
+      log('Patch tradingview iframe');
+
+      iframe.setAttribute('data-okx-patched', 'true');
+      iframe.onmouseover = () => {
+        iframe.focus();
+      };
     }
   }
 }
