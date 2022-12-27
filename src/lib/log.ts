a08578afc54acc61b325fc67aac71118ce417fef
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2022-12-27 18:16:41 +0300

    Update log.ts

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function log(...msg: any) {
-  console.log('[OKX position size calculator]', ...msg);
+  console.log('[Utilities]', ...msg);
 }
